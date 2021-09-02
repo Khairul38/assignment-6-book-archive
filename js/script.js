3784e21437e4a66fede5112f8ec78bc505b159af
Khairul38 <khairulislam38@gmail.com>
2021-09-02 20:34:43 +0600

    update

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ const searchBook = () => {
         emptySearch.appendChild(h5);
     }
     else {
-        const url = `http://openlibrary.org/search.json?q=${searchText}`;
+        const url = `https://openlibrary.org/search.json?q=${searchText}`;
         fetch(url)
             .then(res => res.json())
             .then(data => displaySearchNumber(data))
